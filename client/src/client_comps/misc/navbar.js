a20a303997fbf011ca92ebf27d1cef35abf745e8
Your Name <you@example.com>
2023-03-12 22:34:16 +0200

    ok

---
+++ client/src/client_comps/misc/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+
+  
+  return (
+    <div>
+      <button onClick={()=>{
+
+      }} className='btn'>Open</button>
+    </div>
+    )
+}
