bbe28335d65ab7bcc5a5bb0c994eafb595d7f80f
Your Name <you@example.com>
2023-04-19 17:04:32 +0300

    ok

--- client/src/client_comps/quiz/mainQuiz.js
+++ client/src/client_comps/quiz/mainQuiz.js
@@ -3,14 +3,59 @@ import './mainQuiz.css';
 // Import your icon library here, for example: import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Quiz = ({ questions }) => {
-    const [Questions, setQuestions] = useState(questions);
+    const shuffleArray = (array) => {
+        const newArr = [...array];
+        for (let i = newArr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+        }
+        return newArr;
+    };
+
+    const randomizedQuestions = questions.map((question) => {
+        const shuffledAnswers = shuffleArray(question.answers);
+        const correctIndex = shuffledAnswers.indexOf(question.answers[0]);
+
+        return {
+            ...question,
+            answers: shuffledAnswers,
+            correct: correctIndex,
+        };
+    });
+
+
+    const [Questions, setQuestions] = useState(randomizedQuestions);
     const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [answers, setAnswers] = useState(Array(questions.length).fill(null));
+    const [answers, setAnswers] = useState(JSON.parse(localStorage.getItem('userAnswers')) || Array(questions.length).fill(null));
+    const [showResults, setShowResults] = useState(false);
+    const getStoredAnswers = () => {
+        const storedAnswers = JSON.parse(localStorage.getItem('userAnswers'));
+        const expiration = localStorage.getItem('userAnswersExpiration');
+
+        if (!storedAnswers || !expiration) {
+            return Array(questions.length).fill(null);
+        }
+
+        const currentTime = new Date().getTime();
+        if (currentTime >= Number(expiration)) {
+            localStorage.removeItem('userAnswers');
+            localStorage.removeItem('userAnswersExpiration');
+            return Array(questions.length).fill(null);
+        }
+
+        return storedAnswers;
+    };
+
 
     const handleAnswer = (index, answer) => {
         const newAnswers = [...answers];
         newAnswers[index] = answer;
         setAnswers(newAnswers);
+
+        const currentTime = new Date().getTime();
+        const expirationTime = currentTime + 2 * 60 * 60 * 1000; // 2 hours later
+        localStorage.setItem('userAnswers', JSON.stringify(newAnswers));
+        localStorage.setItem('userAnswersExpiration', expirationTime);
     };
 
     const moveToNextQuestion = () => {
@@ -19,6 +64,19 @@ const Quiz = ({ questions }) => {
 
     const moveToPreviousQuestion = () => {
         setCurrentQuestion(currentQuestion - 1);
+    };
+
+    const checkAnswers = () => {
+        let correctAnswers = 0;
+
+        answers.forEach((answer, index) => {
+            if (answer === Questions[index].correct) {
+                correctAnswers++;
+            }
+        });
+
+        alert(`You got ${correctAnswers} out of ${questions.length} questions right.`);
+        setShowResults(true);
     };
 
     useEffect(() => {
@@ -38,8 +96,8 @@ const Quiz = ({ questions }) => {
                         {Questions[currentQuestion].answers.map((answer, index) => (
                             <button
                                 key={index}
-                                className={`btn btn-${answers[currentQuestion] === index ? 'primary' : 'outline-secondary'}`}
-                                onClick={() => handleAnswer(currentQuestion, index)}
+                                className={`btn btn-${showResults ? (index === Questions[currentQuestion].correct ? 'success' : (answers[currentQuestion] === index ? 'danger' : 'outline-secondary')) : (answers[currentQuestion] === index ? 'primary' : 'outline-secondary')}`}
+                                onClick={() => !showResults && handleAnswer(currentQuestion, index)}
                             >
                                 <h5>{answer}</h5>
                             </button>
@@ -57,13 +115,21 @@ const Quiz = ({ questions }) => {
                     <button className="btn btn-secondary" onClick={moveToPreviousQuestion} disabled={currentQuestion === 0}>
                         Previous
                     </button>
-                    <button
-                        className="btn btn-secondary"
-                        onClick={moveToNextQuestion}
-                        disabled={currentQuestion === questions.length - 1}
-                    >
-                        Next
-                    </button>
+                    {currentQuestion === questions.length - 1 ? (
+                        <button
+                            className="btn btn-secondary"
+                            onClick={checkAnswers}
+                        >
+                            Check Answers
+                        </button>
+                    ) : (
+                        <button
+                            className="btn btn-secondary"
+                            onClick={moveToNextQuestion}
+                        >
+                            Next
+                        </button>
+                    )}
                 </div>
             </div>
         </div>
