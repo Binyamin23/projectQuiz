1a8ae6b691dfa79d09c3ee446bb260d0f106b338
Your Name <you@example.com>
2023-06-07 12:30:15 +0300

    ok

--- client/src/admin_comps/categories/Row.js
+++ client/src/admin_comps/categories/Row.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import AddPictureToCategory from './addImageCategory';
@@ -75,82 +74,4 @@ const Row = ({ item, onXClick, setSelectedCategory, setShowPicture, selectedCate
     )
 }
 
-=======
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useSearchParams } from 'react-router-dom';
-import AddPictureToCategory from './addImageCategory';
-import { Button } from 'react-bootstrap';
-import './categoriesList.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
-const Row = ({ item, onXClick, setSelectedCategory, setShowPicture, selectedCategory, showPicture, isMobile }) => {
-    const [isEditing, setIsEditing] = useState(false);
-    const [isAddImage, setIsAddImage] = useState(false)
-    const [query] = useSearchParams();
-    const { _id, name, url_code, info } = item;
-    useEffect(() => {
-        console.log("%c show", "color:blue")
-    }, [])
-    useEffect(() => {
-        if (query.get("edited") == _id) {
-            setIsEditing(true);
-            setTimeout(() => {
-                setIsEditing(false);
-            }, 20000)
-
-        }
-        ;
-    }, [query.get("edited")])
-
-    const xClick = () => {
-        window.confirm("Delete item?") && onXClick()
-    }
-    const editClick = () => {
-        setSelectedCategory();
-        nav("/admin/categories/edit/" + _id);
-    }
-    const nav = useNavigate();
-    const uploadImage = () => {
-        setIsAddImage(!isAddImage);
-        if (selectedCategory != _id) {
-            setSelectedCategory(_id);
-            setShowPicture(true);
-        }
-        else {
-            if (selectedCategory === _id) {
-                setShowPicture(true);
-            }
-            if (selectedCategory === _id && showPicture) {
-                setShowPicture(false);
-
-            }
-
-        }
-    }
-    return (
-        <tr key={_id}>
-            <td>
-                {name}
-                {isEditing &&
-                    <FontAwesomeIcon className='ms-3' icon={faCircleCheck} flip style={{ color: "#23d138", transition: 'background-color 3s' }} />
-                }
-            </td>
-            <td>{url_code}</td>
-            <td title={info}>{info.substring(0, 15)}...</td>
-            <td>
-                <Button variant='danger' className={isMobile ? 'w-100 mb-2' : 'm-2'} onClick={xClick}>Delete</Button>
-                <Button variant='info' className={isMobile ? 'w-100 mb-2 text-light' : 'm-2 text-light'} onClick={editClick}>Edit</Button>
-                <Button style={isMobile ? { padding: "7.5%" } : null} variant='secondary' className={isMobile ? 'w-100' : 'm-2'} onClick={uploadImage}>Add Image</Button>
-                {isAddImage &&
-                    <AddPictureToCategory
-                        categoryId={_id}
-                        setPictureComp={setSelectedCategory}
-                        close={() => setIsAddImage(false)}
-                    />}
-            </td>
-        </tr>
-    )
-}
-
->>>>>>> 0975c0bd7e9fc34504ce30ee2f8f3faaf1678e83
 export default Row
