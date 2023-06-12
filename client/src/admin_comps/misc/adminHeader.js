88e316d95f219a36385bed1cd673a84ed3b9b205
Your Name <you@example.com>
2023-06-12 15:52:11 +0300

    ok

--- client/src/admin_comps/misc/adminHeader.js
+++ client/src/admin_comps/misc/adminHeader.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../context/createContext';
 
 export default function AdminHeader() {
+
+  const { user, admin } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
   const nav = useNavigate();
+
+  useEffect(()=>{
+    if(user && admin){
+      setLoading(false);
+    }
+    else{
+      nav()
+    }
+  }, [])
 
   return (
     <header className="container-fluid bg-info p-2">
       <div className="container">
         <div className="align-items-center">
           <div className='logo'>
-            <h2 className='h2-no-margin'>Admin</h2>
+            <h2 className='text-dark'>Admin</h2>
+            </div>
+            <div className='logo m-0 p-0'>
+            <h2 style={{ cursor: "pointer" }} className='text-dark' onClick={() => nav('/')}>Quiz</h2>
           </div>
-          <h2  style={{cursor:"pointer"}} className='h2-no-margin text-dark' onClick={() => nav('/')}>Quiz</h2>
-
-
-
           <div className='col-auto'>
             <ul>
               <li><Link to="/admin/categories">Categories</Link></li>
