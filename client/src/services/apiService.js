d168d154d48157b7e98557e1b724d679dda0b930
Your Name <you@example.com>
2023-06-18 12:53:22 +0300

    infoAdd_jaron

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -3,7 +3,6 @@ import axios from "axios"
 // export const API_URL = "https://code-quiz-server.onrender.com";
 export const API_URL = "http://localhost:3008";
 export const API_URL_CLIENT = "http://localhost:3000";
-// export const API_URL = "http://blalba.cyclic.com";
 
 export const TOKEN_KEY = "apps_tok";
 
