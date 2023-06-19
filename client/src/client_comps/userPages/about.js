ac6b7c652ac9039b1717109a2235bcdeb846a969
beny <benybar23@gmail.com>
2023-06-19 13:36:42 +0300

    ok

--- client/src/client_comps/userPages/about.js
+++ client/src/client_comps/userPages/about.js
@@ -2,37 +2,37 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div className="d-flex align-items-center p-5">
-            <div className="container text-center">
-                <div className="row">
-                    <div className="col-lg-8 offset-lg-2">
-                    <h1>Welcome to CodeQuiz!</h1>
-                    <p className="lead mt-4">
-                        At CodeQuiz, our mission is to make learning programming fun, engaging, and accessible to everyone. 
-                        We believe that mastering coding should not be daunting, but instead, an exciting journey filled with 
-                        captivating challenges.
-                    </p>
-                        <p>
-                            CodeQuiz is not just another trivia app; it's a community where beginners, experts, and enthusiasts
-                            meet on the common ground of their passion for coding. Our trivia games are designed to cover a wide
-                            range of programming languages and topics, offering an enjoyable and practical way to sharpen your
-                            skills and deepen your understanding.
-                        </p>
-                        <p>
-                            Our team is comprised of experienced developers and educators, committed to delivering high-quality,
-                            relevant, and up-to-date questions. We constantly strive to keep our trivia database updated and filled
-                            with stimulating challenges to keep our community engaged.
-                        </p>
-                        <p>
-                            With a user-friendly interface, comprehensive ranking system, and a growing community of programmers,
-                            CodeQuiz is more than an app. It's a platform that encourages continuous learning and friendly competition.
-                        </p>
-                        <p className="mt-4">
-                            <strong>Join us today and transform your programming journey into an exciting adventure!</strong>
-                        </p>
+            <div className="d-flex align-items-center p-5">
+                <div className="container text-center">
+                    <div className="row">
+                        <div className="col-lg-8 offset-lg-2">
+                            <h1>Welcome to CodeQuiz!</h1>
+                            <p className="lead mt-4">
+                                At CodeQuiz, our mission is to make learning programming fun, engaging, and accessible to everyone.
+                                We believe that mastering coding should not be daunting, but instead, an exciting journey filled with
+                                captivating challenges.
+                            </p>
+                            <p>
+                                CodeQuiz is not just another trivia app; it's a community where beginners, experts, and enthusiasts
+                                meet on the common ground of their passion for coding. Our trivia games are designed to cover a wide
+                                range of programming languages and topics, offering an enjoyable and practical way to sharpen your
+                                skills and deepen your understanding.
+                            </p>
+                            <p>
+                                Our team is comprised of experienced developers and educators, committed to delivering high-quality,
+                                relevant, and up-to-date questions. We constantly strive to keep our trivia database updated and filled
+                                with stimulating challenges to keep our community engaged.
+                            </p>
+                            <p>
+                                With a user-friendly interface, comprehensive ranking system, and a growing community of programmers,
+                                CodeQuiz is more than an app. It's a platform that encourages continuous learning and friendly competition.
+                            </p>
+                            <p className="mt-4">
+                                <strong>Join us today and transform your programming journey into an exciting adventure!</strong>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
