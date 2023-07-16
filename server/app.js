e51ee745c45d069b53e609b840e12c086f15a1dd
Your Name <you@example.com>
2023-07-16 15:39:37 +0300

    ok

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ const fileUpload = require("express-fileupload");
 require("./db/mongoConnect")
 
 const app = express();
-// מאפשר גם לדומיין שלא קשור לשרת לבצע בקשה 
 app.use(cors());
 
 app.use(fileUpload({
@@ -17,10 +16,8 @@ app.use(fileUpload({
   limits: { fileSize: 1024 * 1024 * 5 }
 }))
 
-// מגדיר לשרת שהוא יכול לקבל מידע מסוג ג'ייסון בבאדי בבקשות שהם לא גט
 app.use(express.json());
 
-// דואג שתקיית פאבליק כל הקבצים בה יהיו חשופים לצד לקוח
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/public/images/categories/:imageName', (req, res) => {
@@ -29,15 +26,11 @@ app.get('/public/images/categories/:imageName', (req, res) => {
   res.sendFile(imagePath);
 });
 
-// פונקציה שמגדירה את כל הראוטים הזמנים באפליקציית
-// צד שרת שלנו
+
 routesInit(app);
 
-// הגדרת שרת עם יכולות אפ שמייצג את האקספרס
 const server = http.createServer(app);
-// משתנה שיגדיר על איזה פורט אנחנו נעבוד
-// אנסה לבדוק אם אנחנו על שרת אמיתי ויאסוף את הפורט משם אם לא ואנחנו לוקאלי יעבוד על 3002
+
 let port = process.env.PORT || 3006;
-// הפעלת השרת והאזנה לפורט המבוקש
 server.listen(port);
 console.log(port)
